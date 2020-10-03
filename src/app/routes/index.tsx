5650d07c7d48cd1ca65baae92bc7d3c6c95ab791
Idrees <dargahidrees@gmail.com>
2020-10-03 20:51:15 +0530

    Basic containers and routing setup

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { createBrowserHistory, History } from 'history';
+import { Router, Switch, Route, Redirect } from 'react-router-dom';
+
+import Login from '../containers/login';
+import SignUp from '../containers/signup';
+
+const history: History = createBrowserHistory();
 
 const RoutesContainer: React.FC = () => {
-  return <></>;
+  let loggedIn: boolean = !!sessionStorage.getItem('accessToken');
+  return (
+    <Router history={history}>
+      <Switch>
+        <Route exact path="/">
+          {loggedIn ? <Redirect to="/dashboard" /> : <Redirect to="/login" />}
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default RoutesContainer;
